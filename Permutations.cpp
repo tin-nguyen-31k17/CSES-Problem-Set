68d4748c20c4af8dad0290fc781469f68bbd522b
Fusioz <trongtin.0301@gmail.com>
2022-12-06 14:13:14 +0700

    Permutations

---
+++ Permutations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int Permutation(int n)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << Permutation;
+}
