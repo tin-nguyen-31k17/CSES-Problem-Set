447495efd1fbf013c581c7c7ae1f1bf72fe04851
Fusioz <trongtin.0301@gmail.com>
2022-12-08 12:44:55 +0700

    update 2 knights

--- Twoknights.cpp
+++ Twoknights.cpp
@@ -11,13 +11,12 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    lli k;
-    cin >> k;
-    for (lli n = 1; n <= k; n++)
-    {
-        cout << n * n * (n * n - 1) / 2 - 4 * (n - 1) * (n - 2) << endl;
-    }
+    int n, k1, k2, ktot, red;
+    cin >> n;
+    k1 = n * n;
+    k2 = k1 - 1;
+    ktot = k1 * k2/2;
+    red = 4*(n-1) * (n-2);
+    cout << ktot-red;
     return 0;
 }
